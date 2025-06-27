db0c71ef2aa31ff96261d5076f64837dcf7b76a7
Min-gyu <lmg07142000@gmail.com>
2025-06-27 17:36:04 +0900

    0627-02

--- js/tool.js
+++ js/tool.js
@@ -1,3 +1,37 @@
+const searchStarterEl = document.querySelector('header .fa-magnifying-glass');
+const searchWrapEl = document.querySelector('.search-wrap');
+const shadowEl = document.querySelector('.search .shadow');
+
+
+searchStarterEl.addEventListener('click', function (event) {
+    event.stopPropagation()
+    if (searchWrapEl.classList.contains('show')) {
+        hideSearch()
+    } else {
+        showSearch()
+    }
+})
+
+window.addEventListener('click', function () {
+    hideSearch()
+})
+searchWrapEl.addEventListener('click', function (event) {
+    event.stopPropagation()
+})
+
+// .shadow 클릭 시 검색창 닫기
+shadowEl.addEventListener('click', function () {
+    hideSearch()
+})
+
+function showSearch() {
+    searchWrapEl.classList.add('show')
+}
+function hideSearch() {
+    searchWrapEl.classList.remove('show')
+}
+
+
 const circles = document.querySelectorAll(".circle");
 const wrap = document.getElementById("wrap");
 const category = document.getElementById("category");
@@ -118,7 +152,7 @@ fetch("./tools.json")
             wrap.appendChild(section);
         });
 
-                // ✅ 스크롤 시 .active 처리
+        // ✅ 스크롤 시 .active 처리
         window.addEventListener("scroll", () => {
             if (isScrollingByClick) return;
 
