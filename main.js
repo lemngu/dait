327f623a941c64d20589ac279507e5585d003de9
gywls00204 <gywls00204@gmai.com>
2025-06-28 12:35:17 +0900

    Start project

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-//메인 검색
-// const headerEl = document.querySelector('header')
-// const searchWrapEl = headerEl.querySelector('.search-wrap')
-// const searchStarterEl = headerEl.querySelector('.search-starter')
-// const searchShadowEl = searchWrapEl.querySelector('.shadow')
-
-// searchStarterEl.addEventListener('click', showSearch)
-// searchShadowEl.addEventListener('click',hideSearch)
-
-
-// function showSearch() {
-//   headerEl.classList.add('searching')
-// }
-// function hideSearch() {
-//   headerEl.classList.remove('searching')
-// }
-
 const searchStarterEl = document.querySelector('header .search-starter')
 const searchWrapEl = document.querySelector('.search-wrap')
 
@@ -67,55 +50,59 @@ function floatingObject(selector, delay, size) {
   });
 }
 floatingObject('.floating1', 1, 15);
-floatingObject('.floating2', .5, 15);
+floatingObject('.floating2', .5, 45);
 floatingObject('.floating3', 1.5, 20);
 floatingObject('.floating4', 1, 15);
-floatingObject('.floating5', .5, 15);
+floatingObject('.floating5', .5, 35);
 floatingObject('.floating6',  1.5, 20);
 floatingObject('.floating7', 1, 15);
 floatingObject('.floating8', .5, 15);
 
 //curation swiper
-new Swiper('.curation .swiper-container', {
-  direction: 'vertical',
-  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
-  spaceBetween: 10, //슬라이드 사이 여백
+new Swiper('.curation-main .swiper-container', {
+  slidesPerView: 1, //한번에 보여줄 슬라이드 개수
+  spaceBetween: 80, //슬라이드 사이 여백
   centeredSlides: true, //1번 슬라이드가 가운데 보이기
   loop: true,
   autoplay: {
-    delay: 5000
+    delay: 3000
   }
 });
 
+new Swiper('.curation-tumb  .swiper-container', {
+  direction: 'horizontal', // 수평 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true, // 반복 재생 여부
+  spaceBetween: 10, // 슬라이드 사이 여백
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+  navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+    prevEl: '.awards .swiper-prev', // 이전 버튼 선택자
+    nextEl: '.awards .swiper-next' // 다음 버튼 선택자
+  }
+})
+
 
 /**
- * 페이지 스크롤에 따른 요소 제어
+ * Promotion 슬라이드 토글 기능
  */
-// 페이지 스크롤에 영향을 받는 요소들을 검색!
-const toTopEl = document.querySelector('#to-top')
-// 페이지에 스크롤 이벤트를 추가!
-// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
-window.addEventListener('scroll', _.throttle(function () {
-  // 페이지 스크롤 위치가 500px이 넘으면.
-  if (window.scrollY > 500) {
-    // 상단으로 스크롤 버튼 보이기!
-    gsap.to(toTopEl, .2, {
-      x: 0
-    })
-
-  // 페이지 스크롤 위치가 500px이 넘지 않으면.
+// 슬라이드 영역 요소 검색!
+const promotionEl = document.querySelector('.promotion')
+// 슬라이드 영역를 토글하는 버튼 검색!
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+// 슬라이드 영역 숨김 여부 기본값!
+let isHidePromotion = false
+// 토글 버튼을 클릭하면,
+promotionToggleBtn.addEventListener('click', function () {
+  // 슬라이드 영역 숨김 여부를 반댓값으로 할당!
+  isHidePromotion = !isHidePromotion
+  // 요소를 숨겨야 하면,
+  if (isHidePromotion) {
+    promotionEl.classList.add('hide')
+  // 요소가 보여야 하면,
   } else {
-    // 상단으로 스크롤 버튼 숨기기!
-    gsap.to(toTopEl, .2, {
-      x: 100
-    })
+    promotionEl.classList.remove('hide')
   }
-}, 300))
-// 상단으로 스크롤 버튼을 클릭하면,
-toTopEl.addEventListener('click', function () {
-  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
-  gsap.to(window, .7, {
-    scrollTo: 0
-  })
 })
+
+
 
